c51ad7afef9ed236adfb42d83f8c9e52472e6f52
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-04 23:42:04 +0700

    set up and add rating

--- src/test/Test2.js
+++ src/test/Test2.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Button } from 'react-native';
-import { LoginButton, LoginManager,AccessToken } from 'react-native-fbsdk';
+import { View, Button, Image } from 'react-native';
+import { LoginButton, LoginManager, AccessToken } from 'react-native-fbsdk';
+import StarRating from 'react-native-star-rating';
 
 const FacebookLogin = () => {
   const handleFacebookLogin = async () => {
@@ -38,6 +39,16 @@ const FacebookLogin = () => {
         onLogoutFinished={() => console.log('Đăng xuất Facebook thành công')}
       />
       <Button title="Đăng nhập Facebook" onPress={handleFacebookLogin} />
+      <StarRating
+        disabled={false} // true nếu bạn muốn người dùng không thể thay đổi đánh giá
+        maxStars={5} // số lượng sao tối đa
+        rating={3} // giá trị đánh giá hiện tại
+       
+        fullStar={require('../assets/icon/ic_star.png')}
+        emptyStar={require('../assets/icon/ic_star.png')}
+        halfStar={require('../assets/icon/ic_star.png')}
+        selectedStar={(rating) => console.log(rating)} // hàm được gọi khi người dùng chọn một số sao
+      />
     </View>
   );
 };
