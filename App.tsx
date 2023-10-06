96d839801514f8a07e6e914015d3f43b052567f8
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-06 23:27:13 +0700

    fix bug

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
-import React, {useState, useRef} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {AppContextProvider} from './src/utils/AppContext';
+import React, { useState, useRef } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppContextProvider } from './src/utils/AppContext';
 import BottomTabs from './src/navigation/BottomNav';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Splash from './src/screens/Begin/Splash';
-import {NativeBaseProvider} from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import Test2 from './src/test/Test2'
+import { Text, LogBox } from 'react-native';
+
 LogBox.ignoreLogs([
   'Require cycle:',
 ]);
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-// return(
-//   <Test2/>
-// )
+  // return(
+  //   <Test2/>
+  // )
   return (
     <AppContextProvider>
       <NavigationContainer>
