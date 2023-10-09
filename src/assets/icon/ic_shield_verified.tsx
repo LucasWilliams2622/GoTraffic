fd712403f01942390fd9bd249723d772393c2f56
Khanh Le <khanh.le@longdan.co.uk>
2023-10-09 17:13:05 +0700

    update icon

--- src/assets/icon/ic_shield_verified.tsx
+++ src/assets/icon/ic_shield_verified.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Svg, {Path} from 'react-native-svg';
-const ShieldIcon = props => (
+import Svg, {SvgProps, Path} from 'react-native-svg';
+const ShieldIcon = (props: SvgProps) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
     width={20}
