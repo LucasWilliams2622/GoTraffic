9a7b1be5589acbb78d327979c9d4e03dcbb68fd1
Khanh Le <khanh.le@longdan.co.uk>
2023-10-10 14:46:49 +0700

    refactored code

--- src/screens/Main/HomeTab/Home.tsx
+++ src/screens/Main/HomeTab/Home.tsx
@@ -17,6 +17,13 @@ import Promotion from '../../../components/Home/Promotion';
 import CarCardItem from '../../../components/Home/CarCardItem';
 import FeaturedLocation from '../../../components/Home/FeaturedLocation';
 import AirportPicking from '../../../components/Home/AirportPicking';
+import {
+  promotionData,
+  carData,
+  featuredLocationData,
+  AirportData,
+  benefitData,
+} from './data/data';
 
 interface RenderListProps<T> {
   data: T[];
@@ -42,157 +49,33 @@ const RenderList: React.FC<RenderListProps<any>> = ({
   />
 );
 
+interface SectionProps {
+  title: string;
+  data: any[];
+  renderItem: ({item}: {item: any}) => JSX.Element;
+  snapToInterval: number;
+}
+
+const Section: React.FC<SectionProps> = ({
+  title,
+  data,
+  renderItem,
+  snapToInterval,
+}) => (
+  <View style={styles.mt20}>
+    <View style={[styles.contentContainer]}>
+      <Text style={appStyle.text18Bold}>{title}</Text>
+    </View>
+    <RenderList
+      data={data}
+      renderItem={renderItem}
+      snapToInterval={snapToInterval}
+    />
+  </View>
+);
+
 const Home: React.FC = () => {
   const navigation = useNavigation();
-  const promotionData = [
-    {
-      id: 1,
-      image: 'https://i.imgur.com/UYiroysl.jpg',
-    },
-    {
-      id: 2,
-      image: 'https://i.imgur.com/UPrs1EWl.jpg',
-    },
-    {
-      id: 3,
-      image: 'https://i.imgur.com/MABUbpDl.jpg',
-    },
-    {
-      id: 4,
-      image: 'https://i.imgur.com/KZsmUi2l.jpg',
-    },
-    {
-      id: 5,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-  ];
-
-  const carData = [
-    {
-      id: 1,
-      title: 'Honda Camry 2018',
-      image:
-        'https://tuvanmuaxe.vn/upload/upload_img/images/Toyota-Camry-2018-gia-xe-tuvanmuaxe-4.jpg',
-      location: 'Quận 1, TP HCM',
-      type: 'Số tự động',
-      price: 1200000,
-      rating: 4.5,
-      totalRide: 15,
-    },
-    {
-      id: 2,
-      title: 'Honda Civic 2022',
-      image:
-        'https://files01.danhgiaxe.com/sxqARZyGxqt_BQww8HR0ig43TX4=/fit-in/1280x0/20220330/danhgiaxe.com-honda-civic-2022-21-023945.jpg',
-      location: 'Quận 1, TP HCM',
-      benefit: 'Giao xe tận nơi',
-      type: 'Số tự động',
-      price: 1234567,
-      rating: 4.5,
-      totalRide: 15,
-    },
-    {
-      id: 3,
-      title: 'Vinfast Lux SA 2.0 2021',
-      image:
-        'https://i2-vnexpress.vnecdn.net/2021/09/18/lux-sa2-1564361912.jpg?w=2400&h=0&q=100&dpr=1&fit=crop&s=Dtfts1hv96Am8dJXVuqe7Q&t=image',
-      location: 'Quận 1, TP HCM',
-      type: 'Số tự động',
-      originalPrice: 1234567,
-      price: 870000,
-      rating: 4.5,
-      totalRide: 15,
-    },
-  ];
-
-  const featuredLocationData = [
-    {
-      id: 1,
-      title: 'Sài Gòn',
-      image: 'https://i.imgur.com/UYiroysl.jpg',
-      totalCar: 3250,
-    },
-    {
-      id: 2,
-      title: 'Hà Nội',
-      image: 'https://i.imgur.com/UPrs1EWl.jpg',
-      totalCar: 1432,
-    },
-    {
-      id: 3,
-      title: 'Đà Nẵng',
-      image: 'https://i.imgur.com/MABUbpDl.jpg',
-      totalCar: 865,
-    },
-    {
-      id: 4,
-      title: 'Nha Trang',
-      image: 'https://i.imgur.com/KZsmUi2l.jpg',
-      totalCar: 721,
-    },
-    {
-      id: 5,
-      title: 'Hải Phòng',
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-      totalCar: 598,
-    },
-  ];
-
-  const AirportData = [
-    {
-      id: 1,
-      title: 'Tân Sơn Nhất',
-      image: 'https://i.imgur.com/UYiroysl.jpg',
-      totalCar: 3250,
-    },
-    {
-      id: 2,
-      title: 'Nội Bài',
-      image: 'https://i.imgur.com/UPrs1EWl.jpg',
-      totalCar: 1432,
-    },
-    {
-      id: 3,
-      title: 'Đà Nẵng',
-      image: 'https://i.imgur.com/MABUbpDl.jpg',
-      totalCar: 865,
-    },
-    {
-      id: 4,
-      title: 'Nha Trang',
-      image: 'https://i.imgur.com/KZsmUi2l.jpg',
-      totalCar: 721,
-    },
-    {
-      id: 5,
-      title: 'Hải Phòng',
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-      totalCar: 598,
-    },
-  ];
-
-  const benefitData = [
-    {
-      id: 1,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-    {
-      id: 2,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-    {
-      id: 3,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-    {
-      id: 4,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-    {
-      id: 5,
-      image: 'https://i.imgur.com/2nCt3Sbl.jpg',
-    },
-  ];
 
   return (
     <ScrollView style={[appStyle.container]}>
@@ -216,70 +99,52 @@ const Home: React.FC = () => {
       </View>
       <Booking navigation={navigation} />
 
-      <View style={styles.mt20}>
-        <View style={[styles.contentContainer]}>
-          <Text style={appStyle.text18Bold}>Chương trình khuyến mãi</Text>
-        </View>
-        <RenderList
-          data={promotionData}
-          renderItem={({item}) => (
-            <Promotion image={item.image} width={300} height={200} />
-          )}
-          snapToInterval={320}
-        />
-      </View>
-      <View style={styles.mt20}>
-        <View style={[styles.contentContainer]}>
-          <Text style={appStyle.text18Bold}>Xe dành cho bạn</Text>
-        </View>
-        <RenderList
-          data={carData}
-          renderItem={({item}) => <CarCardItem {...item} />}
-          snapToInterval={330}
-        />
-      </View>
-      <View style={styles.mt20}>
-        <View style={[styles.contentContainer]}>
-          <Text style={appStyle.text18Bold}>Xe đã xem</Text>
-        </View>
-        <RenderList
-          data={carData}
-          renderItem={({item}) => <CarCardItem {...item} />}
-          snapToInterval={330}
-        />
-      </View>
-      <View style={styles.mt20}>
-        <View style={[styles.contentContainer]}>
-          <Text style={appStyle.text18Bold}>Xe đã xem</Text>
-        </View>
-        <RenderList
-          data={featuredLocationData}
-          renderItem={({item}) => <FeaturedLocation {...item} />}
-          snapToInterval={330}
-        />
-      </View>
-      <View style={styles.mt20}>
-        <View style={[styles.contentContainer]}>
-          <Text style={appStyle.text18Bold}>Xe đã xem</Text>
-        </View>
-        <RenderList
-          data={AirportData}
-          renderItem={({item}) => <AirportPicking {...item} />}
-          snapToInterval={330}
-        />
-      </View>
-      <View style={styles.mt20}>
-        <View style={styles.contentContainer}>
-          <Text style={appStyle.text18Bold}>Ưu điểm của Go Traffic</Text>
-        </View>
-        <RenderList
-          data={benefitData}
-          renderItem={({item}) => (
-            <Promotion image={item.image} width={345} height={130} />
-          )}
-          snapToInterval={365}
-        />
-      </View>
+      <Section
+        title="Chương trình khuyến mãi"
+        data={promotionData}
+        renderItem={({item}) => (
+          <Promotion image={item.image} width={300} height={200} />
+        )}
+        snapToInterval={320}
+      />
+
+      <Section
+        title="Xe dành cho bạn"
+        data={carData}
+        renderItem={({item}) => <CarCardItem {...item} />}
+        snapToInterval={350}
+      />
+
+      <Section
+        title="Xe đã xem"
+        data={carData}
+        renderItem={({item}) => <CarCardItem {...item} />}
+        snapToInterval={350}
+      />
+
+      <Section
+        title="Địa điểm nổi bật"
+        data={featuredLocationData}
+        renderItem={({item}) => <FeaturedLocation {...item} />}
+        snapToInterval={330}
+      />
+
+      <Section
+        title="Đón xe sân bay"
+        data={AirportData}
+        renderItem={({item}) => <AirportPicking {...item} />}
+        snapToInterval={330}
+      />
+
+      <Section
+        title="Ưu điểm của Go Traffic"
+        data={benefitData}
+        renderItem={({item}) => (
+          <Promotion image={item.image} width={345} height={130} />
+        )}
+        snapToInterval={365}
+      />
+
       <View
         style={{
           marginTop: 20,
---
+++ src/screens/Main/HomeTab/data/data.ts
@@ -0,0 +1,149 @@
+export const promotionData = [
+  {
+    id: 1,
+    image: 'https://i.imgur.com/UYiroysl.jpg',
+  },
+  {
+    id: 2,
+    image: 'https://i.imgur.com/UPrs1EWl.jpg',
+  },
+  {
+    id: 3,
+    image: 'https://i.imgur.com/MABUbpDl.jpg',
+  },
+  {
+    id: 4,
+    image: 'https://i.imgur.com/KZsmUi2l.jpg',
+  },
+  {
+    id: 5,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+];
+
+export const carData = [
+  {
+    id: 1,
+    title: 'Honda Camry 2018',
+    image:
+      'https://tuvanmuaxe.vn/upload/upload_img/images/Toyota-Camry-2018-gia-xe-tuvanmuaxe-4.jpg',
+    location: 'Quận 1, TP HCM',
+    type: 'Số tự động',
+    price: 1200000,
+    rating: 4.5,
+    totalRide: 15,
+  },
+  {
+    id: 2,
+    title: 'Honda Civic 2022',
+    image:
+      'https://files01.danhgiaxe.com/sxqARZyGxqt_BQww8HR0ig43TX4=/fit-in/1280x0/20220330/danhgiaxe.com-honda-civic-2022-21-023945.jpg',
+    location: 'Quận 1, TP HCM',
+    benefit: 'Giao xe tận nơi',
+    type: 'Số tự động',
+    price: 1234567,
+    rating: 4.5,
+    totalRide: 15,
+  },
+  {
+    id: 3,
+    title: 'Vinfast Lux SA 2.0 2021',
+    image:
+      'https://i2-vnexpress.vnecdn.net/2021/09/18/lux-sa2-1564361912.jpg?w=2400&h=0&q=100&dpr=1&fit=crop&s=Dtfts1hv96Am8dJXVuqe7Q&t=image',
+    location: 'Quận 1, TP HCM',
+    type: 'Số tự động',
+    originalPrice: 1234567,
+    price: 870000,
+    rating: 4.5,
+    totalRide: 15,
+  },
+];
+
+export const featuredLocationData = [
+  {
+    id: 1,
+    title: 'Sài Gòn',
+    image: 'https://i.imgur.com/UYiroysl.jpg',
+    totalCar: 3250,
+  },
+  {
+    id: 2,
+    title: 'Hà Nội',
+    image: 'https://i.imgur.com/UPrs1EWl.jpg',
+    totalCar: 1432,
+  },
+  {
+    id: 3,
+    title: 'Đà Nẵng',
+    image: 'https://i.imgur.com/MABUbpDl.jpg',
+    totalCar: 865,
+  },
+  {
+    id: 4,
+    title: 'Nha Trang',
+    image: 'https://i.imgur.com/KZsmUi2l.jpg',
+    totalCar: 721,
+  },
+  {
+    id: 5,
+    title: 'Hải Phòng',
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+    totalCar: 598,
+  },
+];
+
+export const AirportData = [
+  {
+    id: 1,
+    title: 'Tân Sơn Nhất',
+    image: 'https://i.imgur.com/UYiroysl.jpg',
+    totalCar: 3250,
+  },
+  {
+    id: 2,
+    title: 'Nội Bài',
+    image: 'https://i.imgur.com/UPrs1EWl.jpg',
+    totalCar: 1432,
+  },
+  {
+    id: 3,
+    title: 'Đà Nẵng',
+    image: 'https://i.imgur.com/MABUbpDl.jpg',
+    totalCar: 865,
+  },
+  {
+    id: 4,
+    title: 'Nha Trang',
+    image: 'https://i.imgur.com/KZsmUi2l.jpg',
+    totalCar: 721,
+  },
+  {
+    id: 5,
+    title: 'Hải Phòng',
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+    totalCar: 598,
+  },
+];
+
+export const benefitData = [
+  {
+    id: 1,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+  {
+    id: 2,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+  {
+    id: 3,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+  {
+    id: 4,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+  {
+    id: 5,
+    image: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+];
