52ed3b6563541acb7e835f32e03a749df4ae325b
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-11-06 00:58:43 +0700

    add image

--- src/components/Home/Home/FeaturedLocation.tsx
+++ src/components/Home/Home/FeaturedLocation.tsx
@@ -20,8 +20,8 @@ const FeaturedLocation = ({title, image, totalCar}: FeaturedLocationProps) => {
         <Text style={[appStyle.text16Bold, {color: COLOR.white}]}>{title}</Text>
         <Text style={{color: COLOR.white}}>{formattedTotalCar} xe</Text>
       </View>
-
-      <FeaturedLocationFrame color={COLOR.fifth} img={image} />
+         
+      <FeaturedLocationFrame color={COLOR.fifth} img={image}  />
     </View>
   );
 };
