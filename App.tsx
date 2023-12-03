45c20e5b8810be36f3bac8921acda65d4848d411
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-12-03 22:36:25 +0700

    speech to text

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import {toastConfig} from './src/configs/ToastConfig';
 import Test2 from './src/test/TestMultiPicker';
 import TestComponent from './src/test/TextSignature';
 import {WebView} from 'react-native-webview';
+import TestVoice from './src/test/TestVoice';
 // Tạo store Redux
 const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
@@ -34,7 +35,8 @@ const App = () => {
             {isLoading ? (
               <Splash setIsLoading={setIsLoading} />
             ) : (
-              <BottomTabs />
+              // <BottomTabs />
+              <TestVoice/>
             )}
             <Toast
               ref={ref => Toast.setRef(ref)}
